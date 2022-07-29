a48b41847034dd6f466208be34dacb8cfba5f3ed
Thomas Eriksen <thomas.eriksen@gmail.com>
2022-07-29 13:21:54 +0200

    Update acm.tf

--- acm.tf
+++ acm.tf
@@ -1,9 +1,8 @@
-resource "aws_acm_certificate" "munkicert" {
-  domain_name       = var.cloudfront_aliases
-  validation_method = "EMAIL"
+resource "aws_acm_certificate" "default" {
+    provider = "aws.acm"  domain_name = "${var.domain}"
 
-  validation_option {
-    domain_name       = var.cloudfront_aliases
-    validation_domain = "bekk.no"
+    subject_alternative_names = ["*.${var.domain}"]
+    validation_method = "EMAIL"  lifecycle {
+      create_before_destroy = true
+    }
   }
-}
