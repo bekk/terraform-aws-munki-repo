2a6930f5e8570f6a64df7c12302f246dce0fa9f0
Thomas Eriksen <thomas.eriksen@gmail.com>
2022-07-29 13:29:59 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      configuration_aliases = [
+         aws.acm,
+      ]
       version = "~> 4.23"
     }
   }
@@ -10,7 +13,4 @@ terraform {
 provider "aws" {
   alias   = "use1"
   region  = "us-east-1"
-}
-provider "aws" {
-  alias = "acm"
 }
