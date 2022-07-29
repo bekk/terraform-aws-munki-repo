e79b3bb61e4bd96a4037a7a07bd022f1cbfb5866
Thomas Eriksen <thomas.eriksen@gmail.com>
2022-07-29 13:08:31 +0200

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -129,7 +129,7 @@ resource "aws_cloudfront_distribution" "www_distribution" {
 
   viewer_certificate {
     #cloudfront_default_certificate = true
-    iam_certificate_id = munki-cert.id
+    acm_certificate_arn = "${aws_acm_certificate.munki-cert.arn}"
   }
 }
 
