65714e33d6d82a441345662a813f832dc4f4e6d6
Thomas Eriksen <thomas.eriksen@gmail.com>
2022-07-29 14:12:27 +0200

    Update acm.tf

--- acm.tf
+++ acm.tf
@@ -3,7 +3,6 @@ resource "aws_acm_certificate" "cert" {
 
     subject_alternative_names = ["*.${var.domain}"]
     validation_method = "EMAIL"
-    ssl_support_method = "sni-only"
     lifecycle {
       create_before_destroy = true
     }
