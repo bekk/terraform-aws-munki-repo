7d0d66776b938f6e7a583c4a6ddcab7af53bf60e
Thomas Eriksen <thomas.eriksen@gmail.com>
2022-07-29 14:22:41 +0200

    Update acm.tf

--- acm.tf
+++ acm.tf
@@ -1,7 +1,6 @@
 resource "aws_acm_certificate" "cert" {  
-    domain_name = "${var.domain}"
+    domain_name = "${var.cloudfront_aliases}"
 
-    subject_alternative_names = ["*.${var.domain}"]
     validation_method = "EMAIL"
     lifecycle {
       create_before_destroy = true
